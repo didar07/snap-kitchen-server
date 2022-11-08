c279d10a808c4a912f26fb839aa5e8137b3d69cc
didar07 <didarul111015@gmail.com>
2022-11-08 14:51:36 +0600

    allservice added

--- index.js
+++ index.js
@@ -14,14 +14,34 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xq1hd1p.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
+const client = new MongoClient(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true,
+
 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('snapKitchen').collection('services')
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+        })
+        app.get('/allservice', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
 
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('snap kitchen serer running',)
