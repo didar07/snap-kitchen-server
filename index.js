2044c39746f42c4406f0a5185db78cc2f5eee300
didar07 <didarul111015@gmail.com>
2022-11-09 12:41:10 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
@@ -19,10 +20,34 @@ const client = new MongoClient(uri, {
 
 });
 
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     const token = authHeader.split(' ')[1]
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//         if (err) {
+//             res.status(403).send({ message: 'unauthorized access' })
+//         }
+//         req.decoded = decoded
+//         next()
+//     })
+// }
+
 async function run() {
     try {
         const serviceCollection = client.db('snapKitchen').collection('services')
         const reviewCollection = client.db('snapKitchen').collection('reviews')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+            res.send({ token })
+        })
+
+
+
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -53,6 +78,12 @@ async function run() {
         })
 
         app.get('/myreviews', async (req, res) => {
+            // const decoded = req.decoded
+            // console.log('inside review', decoded)
+            // if (decoded.email !== req.query.email) {
+            //     res.status(403).send({ message: 'forbidden access' })
+            // }
+
             let query = {}
             if (req.query.email) {
                 query = {
